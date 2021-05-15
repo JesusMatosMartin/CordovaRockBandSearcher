57f4a42a5867791bb89a1e094f2b3aa029506f85
Jesus Matos <jmmartinmatosjesus@gmail.com>
2021-05-15 18:15:37 +0200

    changed init and index js

--- www/js/init.js
+++ www/js/init.js
@@ -1,4 +1,15 @@
+(function($){
+  $(function(){
+ 
+    $('.sidenav').sidenav();
+    $('.tabs').tabs({"swipeable":true});
+    
+  });
+})(jQuery);
 
+document.addEventListener('deviceready', onDeviceReady, false);
+let inputFilter = document.getElementById("artistName");
+ 
 function onDeviceReady() { 
     console.log('Running cordova-' + cordova.platformId + '@' + cordova.version);
     document.getElementById("submitButton").onclick = function() {
@@ -13,4 +24,37 @@ function onDeviceReady() {
     };
 
     $(".carousel")[0].style = "height: 100%; overflow-y: scroll";
+}
+
+function doQuery(filter) {
+  filter = filter.replace(/([^\w_\-,.?])+/g,"").replace(/([\s+])/g," ");
+  
+  $.ajax({
+    method: "GET",
+    url: "https://musicbrainz.org/ws/2/artist?query=" + filter,
+    dataType: "json",
+  }).done(function (msg) {
+    for(var item in msg.artists) {
+      console.log(msg.artists[item]);
+      if (msg.artists[item].disambiguation === undefined) {
+        if (msg.artists[item].area === undefined) {
+          addItem(msg.artists[item].name, " ");
+        } else {
+          addItem(msg.artists[item].name, msg.artists[item].area.name);
+        }
+      } else {
+        addItem(msg.artists[item].name, msg.artists[item].disambiguation);
+      }
+    };
+  }).fail(function () {
+    alert("No se ha encontrado ningun registro el filtro " + filter);
+  });
+}
+
+function addItem(name, description) {
+  $("#list-tab").append('<li class="collection-item avatar"><i class="material-icons circle red">play_arrow</i><span class="title"><b>' + name + '</b></span><p>' + description + '</p></li>');
+}
+
+function clearItems() {
+  $("#list-tab").empty();
 }
